75fd7dc5368cb4b0ac49ea9c4d23052a2700a30a
Dave Kerr <dwmkerr@gmail.com>
2025-09-24 12:58:38 +0200

    fix: update e2e setup script to use new impersonation config

--- .github/actions/setup-e2e/setup-local.sh
+++ .github/actions/setup-e2e/setup-local.sh
@@ -81,7 +81,7 @@ kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/downloa
 echo "=== Installing ARK Controller ==="
 cd "${REPO_ROOT}/ark"
 
-# Deploy controller with permissive impersonation for E2E tests
+# Deploy controller with impersonation enabled for E2E tests
 helm upgrade --install ark-controller ./dist/chart \
   --namespace ark-system \
   --create-namespace \
@@ -90,7 +90,7 @@ helm upgrade --install ark-controller ./dist/chart \
   --set controllerManager.container.image.tag="${ARK_IMAGE_TAG}" \
   --set controllerManager.container.image.pullPolicy=IfNotPresent \
   --set rbac.enable=true \
-  --set rbac.impersonation.allowAll=true
+  --set rbac.impersonation.enabled=true
 
 # Apply coverage configuration if requested
 if [ "${INSTALL_COVERAGE}" = "true" ]; then
