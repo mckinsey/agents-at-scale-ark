254bcd8db8fbd9bb1abbcb12ba0d7c34a964743e
Dave Kerr <dwmkerr@gmail.com>
2025-10-03 09:29:57 +0100

    refactor: remove unused NewAssistantMessageWithName function
    The name is set directly in processAssistantMessage, this helper was never used.

--- ark/internal/genai/types.go
+++ ark/internal/genai/types.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 
 	"github.com/openai/openai-go"
-	"github.com/openai/openai-go/packages/param"
 )
 
 type (
@@ -26,14 +25,6 @@ func NewUserMessage(content string) Message {
 
 func NewAssistantMessage(content string) Message {
 	return Message(openai.AssistantMessage(content))
-}
-
-func NewAssistantMessageWithName(content, name string) Message {
-	msg := openai.AssistantMessage(content)
-	if m := msg.OfAssistant; m != nil {
-		m.Name = param.Opt[string]{Value: name}
-	}
-	return Message(msg)
 }
 
 func ToolMessage[T string | []openai.ChatCompletionContentPartTextParam](content T, toolCallID string) Message {
