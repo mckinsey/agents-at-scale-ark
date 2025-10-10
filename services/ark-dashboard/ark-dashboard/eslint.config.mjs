65c5f8eb9bd6751cd798dbda056fd926610f9504
av3n93rz <daniel.raffer@gmail.com>
2025-10-10 18:41:46 +0200

    feat: enforces stricter TypeScript and import linting rules
    Improves code quality and consistency by enabling additional TypeScript-specific linting rules and import checks.
    Helps prevent unused imports, improper naming conventions, use of 'any', variable shadowing, and import cycles.

--- services/ark-dashboard/ark-dashboard/eslint.config.mjs
+++ services/ark-dashboard/ark-dashboard/eslint.config.mjs
@@ -15,6 +15,9 @@ const eslintConfig = [
   {
     rules: {
       'comma-dangle': ['error', 'never'],
+      'unused-imports/no-unused-imports': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/consistent-type-imports': ['warn'],
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
@@ -22,6 +25,82 @@ const eslintConfig = [
           varsIgnorePattern: '^_',
         },
       ],
+      '@typescript-eslint/no-shadow': ['error'],
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'variableLike',
+          format: ['camelCase'],
+        },
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'function',
+          format: ['camelCase', 'PascalCase'],
+        },
+        {
+          selector: 'parameter',
+          format: ['camelCase', 'PascalCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'enumMember',
+          format: ['UPPER_CASE'],
+        },
+        {
+          selector: 'memberLike',
+          modifiers: ['private'],
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'typeParameter',
+          format: ['PascalCase'],
+          prefix: ['T'],
+        },
+        {
+          selector: 'typeLike',
+          format: ['PascalCase'],
+          custom: {
+            regex: '^[IT][A-Z]',
+            match: false,
+          },
+        },
+        {
+          selector: 'interface',
+          format: ['PascalCase'],
+          custom: {
+            regex: '^[IT][A-Z]',
+            match: false,
+          },
+        },
+      ],
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'no-shadow': 'off',
+      'no-undef': 'off',
+      'import/no-self-import': ['error'],
+      'import/no-cycle': [
+        'warn',
+        {
+          ignoreExternal: true,
+        },
+      ],
+      'import/no-absolute-path': ['error'],
+      'import/newline-after-import': ['warn'],
+      'import/first': ['warn'],
+      eqeqeq: 'error',
+      curly: 'error',
     },
   },
   eslintPluginPrettierRecommended,
