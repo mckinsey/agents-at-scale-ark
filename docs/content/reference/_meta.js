0828d2d2df1618d6b775025febe9756e5574c246
Dave Kerr <dwmkerr@gmail.com>
2025-10-13 09:42:07 +0100

    docs: sort reference menu items alphabetically

--- docs/content/reference/_meta.js
+++ docs/content/reference/_meta.js
@@ -1,10 +1,10 @@
 export default {
   'ark-apis': 'Ark APIs',
+  contributors: 'Contributors',
   crds: 'Custom Resource Definitions',
   evaluations: 'Evaluations',
   'query-execution': 'Query Execution Flow',
   relationships: 'Resource Relationships',
   resources: 'Resources',
-  'upgrading': 'Upgrading',
-  contributors: 'Contributors'
+  'upgrading': 'Upgrading'
 }
