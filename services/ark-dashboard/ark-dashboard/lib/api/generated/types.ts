a520a0143c6e3f84b095bf5a02b7dc668a8a1f22
Daniele Marostica <danielemarostica@gmail.com>
2026-02-05 18:27:49 +0100

    updated types

--- services/ark-dashboard/ark-dashboard/lib/api/generated/types.ts
+++ services/ark-dashboard/ark-dashboard/lib/api/generated/types.ts
@@ -4729,6 +4729,10 @@ export interface components {
         };
         /** ValidationError */
         ValidationError: {
+            /** Context */
+            ctx?: Record<string, never>;
+            /** Input */
+            input?: unknown;
             /** Location */
             loc: (string | number)[];
             /** Message */
