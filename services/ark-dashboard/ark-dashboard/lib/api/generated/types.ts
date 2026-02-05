7777c3e25b4fca4e0e28bd2b307b499b56ec68d6
Paula <Paula_Siano@mckinsey.com>
2026-02-05 16:39:02 -0300

    fix: add optional context and input properties to ValidationError interface in types.ts

--- services/ark-dashboard/ark-dashboard/lib/api/generated/types.ts
+++ services/ark-dashboard/ark-dashboard/lib/api/generated/types.ts
@@ -4713,6 +4713,10 @@ export interface components {
         };
         /** ValidationError */
         ValidationError: {
+            /** Context */
+            ctx?: Record<string, never>;
+            /** Input */
+            input?: unknown;
             /** Location */
             loc: (string | number)[];
             /** Message */
