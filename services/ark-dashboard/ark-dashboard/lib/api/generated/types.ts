ab9163a5765e1e147b5cc52713650910a704d345
Marius Mateescu <marius_mateescu@external.mckinsey.com>
2026-02-06 18:15:02 +0200

    Revert "Merge remote-tracking branch 'origin/fix/branch' into feat/filepreview"
    This reverts commit 4e7dbc8575196dbcaf93a53fdd947dbd8a4407b8, reversing
    changes made to 2c713646959fbc780a3fcb8720a4aeaed0d667fb.

--- services/ark-dashboard/ark-dashboard/lib/api/generated/types.ts
+++ services/ark-dashboard/ark-dashboard/lib/api/generated/types.ts
@@ -4765,10 +4765,6 @@ export interface components {
         };
         /** ValidationError */
         ValidationError: {
-            /** Context */
-            ctx?: Record<string, never>;
-            /** Input */
-            input?: unknown;
             /** Location */
             loc: (string | number)[];
             /** Message */
