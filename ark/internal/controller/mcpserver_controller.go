049fe018ad56902f35f3ad8d1626e6212b120785
Paula <Paula_Siano@mckinsey.com>
2026-08-17 10:18:42 -0300

    chore(deps): update indirect dependencies in go.mod and go.sum

--- ark/internal/controller/mcpserver_controller.go
+++ ark/internal/controller/mcpserver_controller.go
@@ -10,10 +10,12 @@ import (
 	"time"
 
 	"github.com/modelcontextprotocol/go-sdk/mcp"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
